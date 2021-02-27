6921d6dfa43baf84a50ebcdd485ef83974d81cfd
ducdh-fabbi <ducdh@fabbi.com.vn>
2021-02-27 22:30:01 +0700

    fix: sorted

--- src/utils.js
+++ src/utils.js
@@ -77,8 +77,9 @@ export function insertIntoColumn (resolvedImage, dataSet, sorted) {
 
 	if (column) {
 		let images = [...column, resolvedImage];
-		// Fix: always sort by order for Illumy project
-		images = images.sort((a, b) => (a.order < b.order) ? -1 : 1);
+		if (sorted) {
+		    images = images.sort((a, b) => (a.index < b.index) ? -1 : 1);
+		}
 		dataCopy[columnIndex] = images;
 	} else {
 		dataCopy = [...dataCopy, [resolvedImage]];
